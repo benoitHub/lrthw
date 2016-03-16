269f58434b53c03448d367301e4ec8a8db391d35
Benoît Franjou <benoit.laposte.net>
2016-03-17 00:02:11 +0100

    modification fichier budget materiel

--- Material_budget.rb
+++ Material_budget.rb
@@ -4,12 +4,12 @@ puts "Price for the foundation"
 puts "Walls and base: #{4000 + 2000 + 1500 + 250}€"
 
 puts "This is for the wooden part"
-puts "Wood_support: #{3000 + 1700 + 900}€"
-puts "Dors and windows: #{1200 + 850}€"
+puts "Wood_support: #{3000 + 1700.25 + 900}€"
+puts "Doors and windows: #{1200 + 850}€"
 
 puts "This is for the roof covering"
 puts "Tiles: #{1500}€"
 
 puts "and finaly"
 
-puts "Water: #{600}€"
+puts "Water: #{600.27}€"
