d0825f7507bf3d0ec782518189be6e197ef48795
Benoît Franjou <benoit.laposte.net>
2016-03-16 23:57:12 +0100

    ajout fichier materiel ex2

---
+++ Material_budget.rb
@@ -0,0 +1,15 @@
+#Material constructing budget
+
+puts "Price for the foundation"
+puts "Walls and base: #{4000 + 2000 + 1500 + 250}€"
+
+puts "This is for the wooden part"
+puts "Wood_support: #{3000 + 1700 + 900}€"
+puts "Dors and windows: #{1200 + 850}€"
+
+puts "This is for the roof covering"
+puts "Tiles: #{1500}€"
+
+puts "and finaly"
+
+puts "Water: #{600}€"
